e51cd4de34ff9a4d48c1933d3de53f25ffde202c
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-03-02 02:10:01 -0500

    make hello flask route

--- dining.py
+++ dining.py
@@ -1,10 +1,13 @@
 __author__ = 'Jervis'
 
-def test():
-    print 'hello world'
+from flask import Flask
+app = Flask(__name__)
 
 
-def main():
-    test()
-if __name__ == '__main__':
-    main()
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
