7d52138b7c9171fe9895e84a30012e748b19df35
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-03-02 03:18:56 -0500

    update api file

--- api.py
+++ api.py
@@ -1,1 +1,23 @@
-__author__ = 'Jervis'
+__author__ = 'Jervis Muindi'
+
+from flask import Flask
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+
+def db_connect():
+    return MongoClient('localhost', 27017)
+
+def test():
+    file_path = ('dining.json')
+    #load_data()
+
+
+@app.route("/")
+def hello():
+    return "Hello World2"
+
+if __name__ == "__main__":
+    app.run()
+    #test()
