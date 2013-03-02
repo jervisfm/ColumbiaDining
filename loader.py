979832b174a8e3b86bc01bacd5a1ed11a5b3e916
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-03-02 03:15:35 -0500

    finish up loader py script

--- loader.py
+++ loader.py
@@ -1,8 +1,10 @@
 __author__ = 'Jervis Muindi'
 
 import json
+import sys
+import argparse
 from flask import Flask
-import pymongo
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
@@ -15,24 +17,52 @@ def read_entire_file(file_path):
         line = f.readline()
     return result
 
+def db_connect():
+    return MongoClient('localhost', 27017)
+
 def get_json_data(file_path):
     json_text = read_entire_file(file_path)
     j = json.loads(json_text)
     return j['data']
 
-def test():
-    file_path = ('dining.json')
+# Loads data to the local mongo db database
+def load_data():
+    # Connect to MongoDB and Create Dining Database
+    conn = db_connect()
+    db = conn.dining
+    menus = db.menus
+
+    file_path = 'dining.json'
     data = get_json_data(file_path)
+    ids = menus.insert(data)
+
+    print ids
+    print menus.find()
+
+# Retrieves the loaded data
+def show_data():
+    conn = db_connect()
+    menus = conn.dining.menus
+    data = menus.find()
+    data = conn.test.test.find()
     for item in data:
         print item
-    print len(data)
 
-
+def test():
+    file_path = ('dining.json')
+    #load_data()
+    show_data()
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    #app.run()
-    test()
+    if len(sys.argv) > 0:
+        if sys.argv[1] == 'show_data':
+            print 'Showing Loaded Data'
+            show_data()
+            exit(0)
+    print 'Loading Data...'
+    load_data()
+    print 'Data loading complete. run `loader.py show_data` to see the data'
