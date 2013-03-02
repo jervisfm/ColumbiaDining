2c5161aaaff4f60be62cd717eeb0159733f06a15
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-03-02 02:33:25 -0500

    start to read json data

--- dining.py
+++ dining.py
@@ -1,8 +1,26 @@
-__author__ = 'Jervis'
+__author__ = 'Jervis Muindi'
 
+import json
 from flask import Flask
+import pymongo
+
 app = Flask(__name__)
 
+def read_entire_file(file_path):
+    f = open(file_path)
+    line = f.readline()
+    result = ''
+    while line:
+        result += line
+        line = f.readline()
+    return result
+
+
+def test():
+    file_path = ('dining.json')
+    json_text = read_entire_file(file_path)
+    j = json.loads(json_text)
+    print j
 
 
 @app.route("/")
@@ -10,4 +28,5 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    #app.run()
+    test()
